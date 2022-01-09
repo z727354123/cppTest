19ab4729e0fbb17d9ffb50027205848f447ad21e
judy <7273654123@qq.com>
2022-01-09 22:10:07 +0800

    完善

---
+++ base/01_基本操作/03_默认参数/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//#include "../02_extern/sum.h" // 测试 CMakeLists.txt 有没有错
+
+
+int main() {
+
+//    std::cout << sum(1, 20) << std::endl;
+
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
