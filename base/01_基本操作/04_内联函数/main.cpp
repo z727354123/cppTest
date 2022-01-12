acd2a49d08290b8c8dcee6939e47fb18d1d1a0fa
judy <7273654123@qq.com>
2022-01-12 22:24:31 +0800

    更新指针/ cosnt 相关

--- base/01_基本操作/04_内联函数/main.cpp
+++ base/01_基本操作/04_内联函数/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+
 extern "C" {
 #include "test.h"
 }
 
 // 内联函数
-inline int sum (int a, int b) {
+inline int sum(int a, int b) {
     return a + a + b + b;
 }
 // 宏
@@ -12,6 +13,122 @@ inline int sum (int a, int b) {
 
 
 void testTea();
+
+int age = 10;
+
+int &func() {
+    return age;
+}
+
+void swap(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+// 1. 引用测试
+void refTest() {
+    // 1. 返回 引用可以赋值
+    std::cout << "-age = " << age << std::endl;
+    func() = 30;
+    std::cout << "-age = " << age << std::endl;
+    // 2. 交换变量
+    int a = 10;
+    int b = 20;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    swap(a, b);
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+
+
+};
+
+struct Stu {
+    int age;
+};
+
+void constTest() {
+    const Stu str = {20};
+    // 无法修改,
+    // str.age = 30;
+
+    Stu stu2 = {30};
+    Stu *pStu = &stu2;
+    // 可以修改
+    pStu->age = 40;
+    std::cout << "stu2.age = " << stu2.age << std::endl;
+    std::cout << "stu2.& = " << &stu2 << std::endl;
+    const Stu *pStu2 = &stu2;
+    // 不能修改
+    // pStu2->age = 50;
+    // (*pStu2).age = 50;
+    Stu stu3 = *pStu2;
+    stu3.age = 60;
+    std::cout << "stu2.age = " << stu2.age << pStu2->age << std::endl;
+    std::cout << "stu3.age = " << stu3.age << std::endl;
+    std::cout << "stu3.& = " << &stu3 << std::endl;
+    stu2.age = 55;
+    std::cout << "stu2.age = " << stu2.age << pStu2->age << std::endl;
+
+    // 2. 5个指针区别
+    int age = 10;
+    const int *p0 = &age; // *p0 是常量, p0 不是常量
+    int const *p1 = &age; // 同上
+    int *const p2 = &age; // p2 是常量, *p2 不是常量
+    const int *const p3 = &age; // 都是常量
+    int const *const p4 = &age; // 同上
+
+    int age2 = 20;
+    p0 = &age2;
+    // *p0 = 10;
+
+    p1 = &age2;
+    // *p1 = 23;
+
+    // p2 = &age2;
+    *p2 = 30;
+
+    // p3 = &age2;
+    // *p3 = 39;
+
+    // p4 = &age2;
+    // *p4 = 40;
+
+    // 内容其实 都是 一致的
+    std::cout << "*p0 = " << *p0 << std::endl;
+    std::cout << "p0 = " << p0 << std::endl;
+    std::cout << "&p0 = " << &p0 << std::endl;
+
+    std::cout << "*p1 = " << *p1 << std::endl;
+    std::cout << "p1 = " << p1 << std::endl;
+    std::cout << "&p1 = " << &p1 << std::endl;
+
+    std::cout << "*p2 = " << *p2 << std::endl;
+    std::cout << "p2 = " << p2 << std::endl;
+    std::cout << "&p2 = " << &p2 << std::endl;
+
+    std::cout << "*p3 = " << *p3 << std::endl;
+    std::cout << "p3 = " << p3 << std::endl;
+    std::cout << "&p3 = " << &p3 << std::endl;
+
+    std::cout << "*p4 = " << *p4 << std::endl;
+    std::cout << "p4 = " << p4 << std::endl;
+    std::cout << "&p4 = " << &p4 << std::endl;
+
+    // 3. struct 指针
+    Stu oldStu = {10};
+    const Stu * student = &oldStu;
+
+    // 这三个 都是 *stu
+    // *student = oldStu;
+    // (*student).age = 3;
+    // student->age =3;
+
+    student = &oldStu;
+
+
+}
 
 int main() {
 
@@ -60,7 +177,10 @@ int main() {
 
     // 2, 例子
     testTea();
-
+    // 3. 引用用途
+    refTest();
+    // 4. 常量测试
+    constTest();
     return 0;
 }
 
@@ -68,10 +188,10 @@ void testTea() {
     // 老师例子
     int a = 10;
     int b = 20;
-    int& ta = a;
+    int &ta = a;
 
-    int* pa = &a;
-    int*& tpa = pa;
+    int *pa = &a;
+    int *&tpa = pa;
 
 
     std::cout << "a = " << a << std::endl;
@@ -87,8 +207,6 @@ void testTea() {
     std::cout << "*&tpa = " << *&tpa << std::endl;
 
 
-
-
     tpa = &b;
 
     std::cout << "*pa = " << *pa << std::endl;
@@ -100,7 +218,7 @@ void testTea() {
     std::cout << "*&tpa = " << *&tpa << std::endl;
 
     *pa = 30;
-    std::cout << "a = "  << a << std::endl;
+    std::cout << "a = " << a << std::endl;
     std::cout << "b = " << b << std::endl;
 
 
@@ -110,7 +228,7 @@ void testTea() {
     int f = 500;;
     int g = 500;
     int h = 500;
-    long* tp = &c;
+    long *tp = &c;
 
 
     std::cout << "&c =  " << &c << std::endl;
@@ -127,8 +245,6 @@ void testTea() {
     std::cout << "*tp =  " << *tp << std::endl;
     std::cout << "tp  =  " << tp << std::endl;
     std::cout << "&tp =  " << &tp << std::endl;
-
-
 
 
 }
